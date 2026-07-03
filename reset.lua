f77c3657e1c54658bde3a790d9037cce4e7f2f10
zkaiwthbro <pangdiscordlangbakitba@gmail.com>
2026-07-04 01:26:46 +0800

    Increase wait time in infinite loop to 120 seconds

--- reset.lua
+++ reset.lua
@@ -14,7 +14,7 @@ end)
 
 -- Infinite loop (survives respawns since it's a LocalScript)
 while true do
-	task.wait(90) -- 3 minutes
+	task.wait(120) -- 3 minutes
 	
 	if toggled then
 		local character = player.Character
